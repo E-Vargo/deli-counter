b776f0293a2d71d8d17efc6d5238546a93ceb16b
E-Vargo <vargoelliott@gmail.com>
2021-08-12 20:48:51 -0700

    commit tree 1ae796a02348a0ec319a061ec453fd3edea256af

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+
+    if katz_deli.empty?
+        puts "The line is currently empty."
+    else
+        puts  "The line is currently: #{katz_deli.each_with_index {|e, i| "#{i+1}. #{e} " }}"
+    end
+end
+
+def take_a_number
+
+end
+
+def now_serving
+
+end
